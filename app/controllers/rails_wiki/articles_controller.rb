9465fb07bf61aed66e6d35e87b7f0f85304bf6af
Troy Anderson <the.inventor@gmail.com>
2014-09-25 17:55:20 -0600

    dont gsubbang on ar column

--- app/controllers/rails_wiki/articles_controller.rb
+++ app/controllers/rails_wiki/articles_controller.rb
@@ -3,62 +3,64 @@ require_dependency "rails_wiki/application_controller"
 module RailsWiki
   class ArticlesController < ApplicationController
     def index
-     @articles = Article.order(:slug).paginate per_page: 20, page: params[:page]
-   end
+      @articles = Article.order(:slug).paginate per_page: 20, page: params[:page]
+    end
 
 
-   def new
-     @article = Article.new
-   end
+    def new
+      @article = Article.new
+    end
 
-   def show
-     @article = Article.friendly.find(params[:id])
-     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    def show
+      @article = Article.friendly.find(params[:id])
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
 
-     @article.text.gsub!(/\[(?<link>\w[\w\s]+)\]\s/) do
-       link = Regexp.last_match[:link]
-       "[#{link}](/wiki/#{link.parameterize})"
-     end
+      if !@article.text.blank?
+        article_body_after_wiki_links = @article.text.gsub(/\[(?<link>\w[\w\s]+)\]\s/) do
+          link = Regexp.last_match[:link]
+          "[#{link}](/wiki/#{link.parameterize})"
+        end
+      end
 
-     @markdown_body = markdown.render(@article.text || '')
-     @versions_available = true if @article.versions.count > 1
+      @markdown_body = markdown.render(article_body_after_wiki_links || '')
+      @versions_available = true if @article.versions.count > 1
 
-   rescue ActiveRecord::RecordNotFound => ex
-     @article = Article.new(title: params[:id],text: "Article wasn't found, want to create one?")
-   end
+    rescue ActiveRecord::RecordNotFound => ex
+      @article = Article.new(title: params[:id],text: "Article wasn't found, want to create one?")
+    end
 
-   def create
-     @article = Article.new(article_params)
+    def create
+      @article = Article.new(article_params)
 
-     @article.save
-     redirect_to @article
-   end
+      @article.save
+      redirect_to @article
+    end
 
-   def edit
-     @article = Article.friendly.find(params[:id])
-   end
+    def edit
+      @article = Article.friendly.find(params[:id])
+    end
 
-   def versions
-     require 'differ'
-     Differ.format = :html
+    def versions
+      require 'differ'
+      Differ.format = :html
 
 
-     @article = Article.friendly.find(params[:id])
-   end
+      @article = Article.friendly.find(params[:id])
+    end
 
-   def update
-     @article = Article.friendly.find(params[:id])
-     if @article.update_attributes article_params
-       redirect_to @article, notice: 'All good!'
-     else
-       render :edit
-     end
-   end
+    def update
+      @article = Article.friendly.find(params[:id])
+      if @article.update_attributes article_params
+        redirect_to @article, notice: 'All good!'
+      else
+        render :edit
+      end
+    end
 
-   private
+    private
 
-   def article_params
-     params.require(:article).permit(:title, :text, :version, :references, :protected)
-   end
+    def article_params
+      params.require(:article).permit(:title, :text, :version, :references, :protected)
+    end
   end
 end
